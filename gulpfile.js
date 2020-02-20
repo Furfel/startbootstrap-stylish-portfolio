24545e199efc15ec06bce1aa28569901ea49c0b3
Furfel <bugs@furfel.com>
2020-02-20 15:59:56 +0100

    Menu generaated with gulp-template and json

--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,9 @@ const plumber = require("gulp-plumber");
 const rename = require("gulp-rename");
 const sass = require("gulp-sass");
 const uglify = require("gulp-uglify");
+const template = require("gulp-template");
+const data = require("gulp-data");
+const fs = require("fs");
 
 // Load package.json for banner
 const pkg = require('./package.json');
@@ -25,11 +28,13 @@ const banner = ['/*!\n',
   '\n'
 ].join('');
 
+const dest_build = "./build/";
+
 // BrowserSync
 function browserSync(done) {
   browsersync.init({
     server: {
-      baseDir: "./"
+      baseDir: dest_build
     },
     port: 3000
   });
@@ -44,34 +49,34 @@ function browserSyncReload(done) {
 
 // Clean vendor
 function clean() {
-  return del(["./vendor/"]);
+  return del([dest_build + "vendor/"]);
 }
 
 // Bring third party dependencies from node_modules into vendor directory
 function modules() {
   // Bootstrap
   var bootstrap = gulp.src('./node_modules/bootstrap/dist/**/*')
-    .pipe(gulp.dest('./vendor/bootstrap'));
+    .pipe(gulp.dest(dest_build + 'vendor/bootstrap'));
   // Font Awesome CSS
   var fontAwesomeCSS = gulp.src('./node_modules/@fortawesome/fontawesome-free/css/**/*')
-    .pipe(gulp.dest('./vendor/fontawesome-free/css'));
+    .pipe(gulp.dest(dest_build + 'vendor/fontawesome-free/css'));
   // Font Awesome Webfonts
   var fontAwesomeWebfonts = gulp.src('./node_modules/@fortawesome/fontawesome-free/webfonts/**/*')
-    .pipe(gulp.dest('./vendor/fontawesome-free/webfonts'));
+    .pipe(gulp.dest(dest_build + 'vendor/fontawesome-free/webfonts'));
   // jQuery Easing
   var jqueryEasing = gulp.src('./node_modules/jquery.easing/*.js')
-    .pipe(gulp.dest('./vendor/jquery-easing'));
+    .pipe(gulp.dest(dest_build + 'vendor/jquery-easing'));
   // jQuery
   var jquery = gulp.src([
       './node_modules/jquery/dist/*',
       '!./node_modules/jquery/dist/core.js'
     ])
-    .pipe(gulp.dest('./vendor/jquery'));
+    .pipe(gulp.dest(dest_build + 'vendor/jquery'));
   // Simple Line Icons
   var simpleLineIconsFonts = gulp.src('./node_modules/simple-line-icons/fonts/**')
-    .pipe(gulp.dest('./vendor/simple-line-icons/fonts'));
+    .pipe(gulp.dest(dest_build + 'vendor/simple-line-icons/fonts'));
   var simpleLineIconsCSS = gulp.src('./node_modules/simple-line-icons/css/**')
-    .pipe(gulp.dest('./vendor/simple-line-icons/css'));
+    .pipe(gulp.dest(dest_build + 'vendor/simple-line-icons/css'));
   return merge(bootstrap, fontAwesomeCSS, fontAwesomeWebfonts, jquery, jqueryEasing, simpleLineIconsFonts, simpleLineIconsCSS);
 }
 
@@ -91,12 +96,27 @@ function css() {
     .pipe(header(banner, {
       pkg: pkg
     }))
-    .pipe(gulp.dest("./css"))
+    .pipe(gulp.dest(dest_build + "css"))
     .pipe(rename({
       suffix: ".min"
     }))
     .pipe(cleanCSS())
-    .pipe(gulp.dest("./css"))
+    .pipe(gulp.dest(dest_build + "css"))
+    .pipe(browsersync.stream());
+}
+
+//HTML task
+function html() {
+  return gulp
+    .src("./*.html")
+    .pipe(data(function(file) {
+      var merged = {};
+      var menu = JSON.parse(fs.readFileSync("./menu.json"));
+      Object.keys(menu).forEach(k => merged[k] = menu[k]);
+      return merged;
+    }))
+    .pipe(template())
+    .pipe(gulp.dest(dest_build))
     .pipe(browsersync.stream());
 }
 
@@ -114,7 +134,7 @@ function js() {
     .pipe(rename({
       suffix: '.min'
     }))
-    .pipe(gulp.dest('./js'))
+    .pipe(gulp.dest(dest_build + 'js'))
     .pipe(browsersync.stream());
 }
 
@@ -122,12 +142,12 @@ function js() {
 function watchFiles() {
   gulp.watch("./scss/**/*", css);
   gulp.watch("./js/**/*", js);
-  gulp.watch("./**/*.html", browserSyncReload);
+  gulp.watch("./**/*.html", gulp.series(html, browserSyncReload));
 }
 
 // Define complex tasks
 const vendor = gulp.series(clean, modules);
-const build = gulp.series(vendor, gulp.parallel(css, js));
+const build = gulp.series(vendor, gulp.parallel(html, css, js));
 const watch = gulp.series(build, gulp.parallel(watchFiles, browserSync));
 
 // Export tasks
@@ -138,3 +158,4 @@ exports.vendor = vendor;
 exports.build = build;
 exports.watch = watch;
 exports.default = build;
+exports.html = html;
---
+++ posts/Untitled.md
@@ -0,0 +1,3 @@
+Hello world
+---
+This is my first post. Hooray! *however* __as it is__ a very important post I would like to remind you about.
